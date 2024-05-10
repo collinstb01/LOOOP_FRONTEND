76e6a822956fcc0bba08b618d95c2418399f3387
collinstb01@gmail.com <collinstb01@gmail.com>
2024-05-10 11:37:49 +0100

    connected backend

--- src/sections/Home/Hero.jsx
+++ src/sections/Home/Hero.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import Buttons from "../../components/Buttons";
 import { useWallet } from "@solana/wallet-adapter-react";
